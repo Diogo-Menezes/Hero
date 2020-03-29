250ccba91fd0a17a30aee12da0026b8c4881a19f
Diogo Menezes <diogo444@hotmail.com>
2020-03-29 10:52:06 +0200

    Database changes, controllers refactor

--- backend/src/controllers/NgoController.js
+++ backend/src/controllers/NgoController.js
@@ -3,14 +3,14 @@ const generateUniqueId = require('../utils/generateUniqueId')
 
 module.exports = {
   async index(request, response) {
-    const ongs = await connection('ongs').select('*');
-    return response.json(ongs);
+    const ngos = await connection('ngos').select('*');
+    return response.json(ngos);
   },
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
     const id = generateUniqueId()
 
-    await connection('ongs').insert({
+    await connection('ngos').insert({
       id,
       name,
       email,

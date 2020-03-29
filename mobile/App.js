fd94e79c71534c9c4842c1ff1c92472b3dc19724
Diogo Menezes <diogo444@hotmail.com>
2020-03-29 11:21:22 +0200

    rollback app.js

--- mobile/App.js
+++ mobile/App.js
@@ -2,12 +2,8 @@ import 'intl';
 import 'intl/locale-data/jsonp/pt-PT';
 
 import React from 'react';
-import { Text, View } from 'react-native';
 import Routes from './routes';
-import Login from './src/pages/Login/index';
-import Incidents from './src/pages/Incidents/index';
 
 export default function App() {
-  return <Login />;
-  //  <Routes />;
+  return <Routes />;
 }

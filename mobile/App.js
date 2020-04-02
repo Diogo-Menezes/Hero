ac25ac9a5c32deca3b8d7f842b168e61f66a4c5a
Diogo Menezes <diogo444@hotmail.com>
2020-04-02 10:49:53 +0200

    Add badges

--- mobile/App.js
+++ mobile/App.js
@@ -8,7 +8,7 @@ import { View, StatusBar } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <StatusBar barStyle='dark-content'/>
+      <StatusBar barStyle='light-content'/>
       <Routes />
     </View>
   );
